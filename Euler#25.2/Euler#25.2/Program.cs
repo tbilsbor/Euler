07565da9ffe698eab6679e9cf41c39a4a806590c
Todd Bilsborough <todd.bilsborough@gmail.com>
2018-06-30 13:53:22 -0600

    New uploads
    Initialization of 26 and brute force solution for 25

---
+++ Euler#25.2/Euler#25.2/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+	Hence the first 12 terms will be:
+
+	F1 = 1
+	F2 = 1
+	F3 = 2
+	F4 = 3
+	F5 = 5
+	F6 = 8
+	F7 = 13
+	F8 = 21
+	F9 = 34
+	F10 = 55
+	F11 = 89
+	F12 = 144
+	The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+*/
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace Euler25
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Running!");
+			Stopwatch stopWatch = new Stopwatch ();
+			stopWatch.Start ();
+
+			int DIGITS = 1000; // Find the first Fibonacci number with this many digits
+
+			List<BigInteger> fibonacci = new List<BigInteger> (5000); // List of Fibonacci numbers
+			List<int> fibDigits = new List<int> (5000); // Digits in Fibonacci numbers
+
+			// Initialize Fibonacci numbers and their digits
+
+			fibonacci.Add (1);
+			fibonacci.Add (1);
+			fibDigits.Add (1);
+			fibDigits.Add (1);
+
+			int d = 0;
+			int index = -1;
+			for (int i = 2; d <= DIGITS; i++) {
+				fibonacci.Add (fibonacci [i - 1] + fibonacci [i - 2]);
+				d = fibonacci [i].ToString ().Length;
+				if (d >= 1000) {
+					index = i + 1; // +1 because the index of the first Fibonacci number is 0
+					break;
+				}
+			}
+
+			stopWatch.Stop ();
+			Console.WriteLine (index + " found in " + stopWatch.ElapsedMilliseconds + " milliseconds");
+
+		}
+	}
+}
--- Euler#26/Euler#26/Program.cs
+++ Euler#26/Euler#26/Program.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+1/2	= 	0.5
+1/3	= 	0.(3)
+1/4	= 	0.25
+1/5	= 	0.2
+1/6	= 	0.1(6)
+1/7	= 	0.(142857)
+1/8	= 	0.125
+1/9	= 	0.(1)
+1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+
+Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+*/
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Euler26
 {
@@ -6,7 +27,57 @@ namespace Euler26
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+
+			/* Investigation
+
+			List<double> reciprocals = new List<double> ();
+
+			reciprocals.Add (-1);
+			reciprocals.Add (-1);
+
+			for (int d = 2; d < 100; d++) {
+				reciprocals.Add ((double) 1 / (double) d);
+				Console.WriteLine (d + ": {0:F28}", reciprocals [d]);
+			}
+			
+			*/
+
+			Console.WriteLine ("Running!");
+			Stopwatch stopWatch = new Stopwatch ();
+			stopWatch.Start ();
+
+			int UPPER_BOUND = 1000;
+
+			int longest = -1;
+			int longestVal = -1;
+			int l = -1;
+			for (int d = 2; d < UPPER_BOUND; d++) {
+				l = CycleLength (d);
+				if (l > longestVal) {
+					longestVal = l;
+					longest = d;
+				}
+			}
+
+			stopWatch.Stop ();
+			Console.WriteLine (longest + " found with a cycle of " + longestVal + " in " + 
+				stopWatch.ElapsedMilliseconds + " milliseconds");
+		}
+
+		private static int CycleLength (int denominator) {
+			List<int> digits = new List<int> ();
+			StringBuilder s = new StringBuilder ();
+			int numerator = 10;
+			int quotient = -1;
+			for (int digit = 0; digit < 1000; digit++) {
+				quotient = numerator / denominator;
+				digits.Add (quotient);
+				s.Append (quotient);
+				numerator -= digits [digit] * denominator;
+				numerator *= 10;
+			}
+
+			return 0;
 		}
 	}
 }
