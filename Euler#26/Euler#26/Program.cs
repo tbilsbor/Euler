d726c86cf6273f901984ddfd2dfa5d3b1597ce5c
Todd Bilsborough <todd.bilsborough@gmail.com>
2018-07-02 19:41:06 -0600

    26 new approach

--- Euler#26/Euler#26/Program.cs
+++ Euler#26/Euler#26/Program.cs
@@ -21,6 +21,7 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Numerics;
 
 namespace Euler26
 {
@@ -48,15 +49,33 @@ namespace Euler26
 			stopWatch.Start ();
 
 			int UPPER_BOUND = 1000; // Check reciprocals up to this number
+			List<int> primes = new List<int> ();
 
+			//prime bootstrapper
+			primes.Add(2);
+			primes.Add (3);
+			bool divisible = false;
+			for (int n = 3; n <= UPPER_BOUND; n += 2) {
+				for (int p = 4; p * p <= n; p++) {
+					if (n % p == 0) {
+						divisible = true;
+						break;
+					}
+				}
+				if (!divisible) {
+					primes.Add (n);
+				}
+				divisible = false;
+			}
+				
 			int longest = -1; // Denominator of the reciprocal with the longest cycle
 			int longestVal = -1; // Cycle length of the reciprocal with the longest cycle
 			int l = -1;
-			for (int d = 2; d < UPPER_BOUND; d++) {
-				l = CycleLength (d);
+			foreach (int p in primes) {
+				l = CycleLength (p);
 				if (l > longestVal) {
 					longestVal = l;
-					longest = d;
+					longest = p;
 				}
 			}
 
@@ -64,7 +83,41 @@ namespace Euler26
 			Console.WriteLine (longest + " found with a cycle of " + longestVal + " in " + 
 				stopWatch.ElapsedMilliseconds + " milliseconds");
 		}
-			
+
+		private static int CycleLength (int denominator) {
+			BigInteger pTen = 1;
+			// Cycle of a decimal of a fraction in lowest terms with prime denominator = 
+			// lowest k s.t. 10^k % denominator = 1
+			for (int k = 1; k < denominator; k++) {
+				pTen *= 10;
+				if (pTen % denominator == 1) {
+					return k;
+				}
+			}
+					
+			return -1;
+		}
+
+		/*
+		private static int CycleLength (int denominator) {
+			BigInteger pTen = 1;
+			for (int k = 1; k < denominator; k++) {
+				for (int exp = 1; exp <= k; exp++) {
+					pTen *= 10;
+				}
+				// Cycle of a decimal of a fraction in lowest terms with prime denominator = 
+				// lowest k s.t. 10^k % denominator = 1
+				if (pTen % denominator == 1) {
+					return k;
+				}
+				pTen = 1;
+			}
+
+			return -1;
+		}
+		*/
+
+		/*
 		private static int CycleLength (int denominator) {
 			if (denominator % 2 == 0 || denominator % 5 == 0) { // fast test for divisibility by 2 or 5
 				return -1;
@@ -97,5 +150,6 @@ namespace Euler26
 			return cycleLength;
 
 		}
+		*/
 	}
 }
