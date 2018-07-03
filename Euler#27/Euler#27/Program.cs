48c2d622b69684e88753985ea21c4b6c223f42e2
Todd Bilsborough <todd.bilsborough@gmail.com>
2018-07-03 16:34:19 -0600

    27 optimized brute force

--- Euler#27/Euler#27/Program.cs
+++ Euler#27/Euler#27/Program.cs
@@ -41,6 +41,7 @@ namespace Euler27
 			}
 
 			//brute force
+			/*
 			int n = 0;
 			for (int a = -999; a < 1000; a++) {
 				for (int b = -1000; b <= 1000; b++) {
@@ -56,11 +57,38 @@ namespace Euler27
 					n = 0;
 				}
 			}
+			*/
+
+			//optimized brute force
+			int n = 0;
+			for (int a = -999; a < 1000; a += 2) { // a must be odd so that n^2 + an + b produces a prime when n = 1; 
+			// Except for the even prime 2, but that would have to be the first prime produced 
+			// so n would = 0 and n = 0 cancels out a and the formula can = 2 if b = 2;
+				for (int bIndex = 0; primes [bIndex] <= 1000; bIndex++) { // b must be prime so that n^2 + an + b produces a prime when n = 0
+					int b = primes [bIndex]; 
+					for (int iteration = 0; iteration <= 1; iteration++) { // Run code once each for positive and negative value of b
+						if (iteration == 1) {
+							b *= -1;
+						}
+						while (n != b && n != b * -1 && primes.Contains ((n * n) + (a * n) + b)) {
+							n++;
+						}
+						n -= 1;
+						if (n > primesFound) {
+							primesFound = n + 1;
+							aMax = a;
+							bMax = b;
+						}
+						n = 0;
+						b = primes [bIndex] * -1; // ...then the negative value of b
+					}
+				}
+			}
 
 			product = aMax * bMax;
 
 			stopWatch.Stop ();
-			Console.WriteLine ("{0} is the product of {1} and {2}; n^2 + {1}n + {2} produces {3} consecutive primes", product, aMax, bMax, primesFound);
+			Console.WriteLine ("{0} is the product of {1} and {2}\nn^2 + {1}n + {2} produces {3} consecutive primes", product, aMax, bMax, primesFound);
 			Console.WriteLine ("Answer found in {0} milliseconds", stopWatch.ElapsedMilliseconds);
 		}
 	}
