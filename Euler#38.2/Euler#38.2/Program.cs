300e60594b6e4bc7057e618949068b884d9e5858
Todd Bilsborough <todd.bilsborough@gmail.com>
2018-08-12 13:46:17 -0600

    38 comments and cleanup

--- Euler#38.2/Euler#38.2/Program.cs
+++ Euler#38.2/Euler#38.2/Program.cs
@@ -32,28 +32,29 @@ namespace Euler38
 			Stopwatch stopWatch = new Stopwatch ();
 			stopWatch.Start ();
 
-			int largest = 1;
-			int digits = 9;
-			List<int> panSeq = new List<int> (digits);
-			for (int n = 1; n <= digits; n++) {
+			int largest = 1; // Index referencing largest (i.e. first) value found
+			int digits = 9; // Number of digits in the sequence
+			List<int> panSeq = new List<int> (digits); // The pandigital sequence
+			for (int n = 1; n <= digits; n++) { // Populate based on number of digits
 				panSeq.Add (n);
 			}
-			Permutations permutations = new Permutations (panSeq);
-			permutations.GetPermutations ();
+			Permutations permutations = new Permutations (panSeq); // Instantiate object
+			permutations.GetPermutations (); // Generate the permutations
 
-			for (int i = permutations.Count - 1; i >= 0; i--) {
-				List<int> permutation = permutations.GetPermutation (i);
-				if (!TestOne (permutation)
+			for (int i = permutations.Count - 1; i >= 0; i--) { // For each permutation, starting at the end
+				List<int> permutation = permutations.GetPermutation (i); // Grab it from the object
+				if (!TestOne (permutation) // Test all the configurations
 					&& !TestTwo (permutation)
 					&& !TestThree (permutation)
 					&& !TestFour (permutation)) {
-					continue;
-				} else {
-					largest = i;
+					continue; // Didn't pass any; move onto the next
+				} else { // One of them passed
+					largest = i; // Set the index that passed
 					break;
 				}
 			}
-				
+
+			// Grab the permutation using the index and turn it into an integer for printout
 			List<int> largestPermutation = permutations.GetPermutation (largest);
 			StringBuilder largestString = new StringBuilder ();
 			foreach (int digit in largestPermutation) {
@@ -68,97 +69,78 @@ namespace Euler38
 
 		public class Permutations
 		{
-			private List<int> panSeq = new List<int> ();
-			private List<List<int>> permutations = new List<List<int>> ();
+			private List<int> panSeq = new List<int> (); // The pandigital sequence
+			private List<List<int>> permutations = new List<List<int>> (); // List of permutations
 
+			// Constructor
 			public Permutations (List<int> s_) {
-				foreach (int digit in s_) {
+				foreach (int digit in s_) { // Copy sequence from argument to local list
 					panSeq.Add (digit);
 				}
 			}
 
-			public int Count {
+			public int Count { // Get the number of permutations
 				get {
 					return permutations.Count;
 				}
 			}
 
-			private int Factorial (int n_) {
-				int number = n_;
-				for (int m = number; m >= 2; m--) {
-					number *= m;
-				}
-				return number;
-			}
-
-			public List<int> GetPermutation (int i_) {
+			public List<int> GetPermutation (int i_) { // Get a permutation specified by index
 				return permutations [i_];
 			}
 
-			public void GetPermutations () {
+			public void GetPermutations () { // Initial call to GetPermutations
 				GetPermutations (panSeq, null);
 			}
 
 			private void GetPermutations (List<int> s_, List<int> p_) {
-				List<int> sequence = new List<int> ();
-				foreach (int d in s_) {
+				List<int> sequence = new List<int> (); // Subset of the pandigital sequence, or subset of a subset
+				foreach (int d in s_) { // Copy argument to subset list
 					sequence.Add (d);
 				}
-				List<int> permutation = new List<int> ();
-				if (p_ != null) {
-					foreach (int d in p_) {
+				List<int> permutation = new List<int> (); // Holds the current permutation as it's generated
+				if (p_ != null) { // If a new permutation has already been started...
+					foreach (int d in p_) { // Copy it over from the argument
 						permutation.Add (d);
 					}
-
 				}
-				foreach (int d in sequence) {
-					permutation.Add (d);
-					List<int> subSeq = new List<int> ();
-					foreach (int d2 in sequence) {
+				foreach (int d in sequence) { // Iterate over the subset sequence
+					permutation.Add (d); // Grab the number and add it to the permutation
+					// Holds the new subset, which is the old subset minus the number grabbed
+					List<int> subSeq = new List<int> (); 
+					foreach (int d2 in sequence) { // Copy over the numbers that aren't in the permutation builder
 						if (d2 != d) {
 							subSeq.Add (d2);
 						}
 					}
-					if (subSeq.Count == 0) {
-						permutations.Add (new List<int> ());
-						foreach (int d3 in permutation) {
+					if (subSeq.Count == 0) { // If that runs the list out of numbers
+						permutations.Add (new List<int> ()); 
+						foreach (int d3 in permutation) { // Add the finalized permutation to the big list
 							permutations [permutations.Count - 1].Add (d3);
 						}
 						break;
-					} else {
-						GetPermutations (subSeq, permutation);
-						permutation.RemoveAt (permutation.Count - 1);
+					} else { // Otherwise...
+						GetPermutations (subSeq, permutation); // Recurse!
+						// Pop the last number off the sequence so that it can be replaced in the next iteration
+						permutation.RemoveAt (permutation.Count - 1); 
 					}
 				}
 			}
 		}
 
+		// Test the first number against the second and third, fourth and fifth, etc.
 		public static Boolean TestOne (List<int> permutation) 
 		{
 			int testVal = permutation [0];
-			int compareTo = permutation [1] * 10;
-			compareTo += permutation [2];
-			int product = testVal * 2;
-			if (product != compareTo) {
-				return false;
-			} 
-			compareTo = permutation [3] * 10;
-			compareTo += permutation [4];
-			product = testVal * 3;
-			if (product != compareTo) {
-				return false;
-			} 
-			compareTo = permutation [5] * 10;
-			compareTo += permutation [6];
-			product = testVal * 4;
-			if (product != compareTo) {
-				return false;
-			}
-			compareTo = permutation [7] * 10;
-			compareTo += permutation [8];
-			product = testVal * 5;
-			if (product != compareTo) {
-				return false;
+			int compareTo;
+			int product;
+			for (int m = 2; m <= 5; m++) {
+				compareTo = permutation [m - 1] * 10;
+				compareTo += permutation [m];
+				product = testVal * m;
+				if (product != compareTo) {
+					return false;
+				}
 			}
 			return true;
 		}
